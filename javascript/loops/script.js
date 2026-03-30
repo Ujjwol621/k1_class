3a97c258cbe2ce6ac33de0441cd17618f6ae6ed9
Ujjwol Tamang <ujjwoltmg47@gmail.com>
2026-03-30 12:32:49 +0545

    added prime number generation

--- javascript/loops/script.js
+++ javascript/loops/script.js
@@ -17,11 +17,51 @@
 // }
 
 //WAP program that counts the total number of even numbers between 1 to 100
-let count=0;
-for(let i=1; i<=100; i++){
-    if(i%2==0){
-        console.log(i);
-        count++;
-    }
-}
-console.log(`Total even numbers between 1 and 100 are ${count}`);
+// let count=0;
+// for(let i=1; i<=100; i++){
+//     if(i%2==0){
+//         console.log(i);
+//         count++;
+//     }
+// }
+// console.log(`Total even numbers between 1 and 100 are ${count}`);
+
+// print first 5 prime numbers
+// let count=0;
+// for(let i=2; count<5; i++){
+//     let isPrime=true;
+//     for(let j=2; j<i; j++){
+//         if(i%j==0){
+//             isPrime=false;
+//             break;
+//         }
+//     }
+//     if(isPrime){
+//         console.log(i);
+//         count++;
+//     }
+// }
+
+// 2. while loop
+// let i=1;
+// while(i<=20){
+//     console.log(i);
+//     i++;
+// }
+
+// let i=20;
+// while(i>0){
+//     console.log(i);
+//     i--;
+// }
+
+// 3. do while loop
+// let i=9;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10);
+
+// difference between while and do while loop
+// in while loop, the condition is checked before executing the code block, so if the condition is false at the beginning, the code block will not execute at all.
+// in do while loop, the code block is executed at least once before checking the condition, so even if the condition is false at the beginning, the code block will execute at least once.
