00d7937e951d09935e972077531c5680a77d45c9
Ujjwol Tamang <ujjwoltmg47@gmail.com>
2026-04-23 12:39:44 +0545

    tailwindcss set up, learned box-model(border,color,padding,margin,bg)

---
+++ tailwind_css/src/App.jsx
@@ -0,0 +1,45 @@
+function App() {
+  return (
+    // <div className="border">App</div> // 1px
+    // <div className="border-0">App</div> // 0px
+    // <div className="border-2">App</div> // 2px
+    // <div className="border-4">App</div> // 4px
+    // <div className="border-8">App</div> // 8px
+    // <div className="border-[20px]">App</div> // 20px custom border width
+    // <div className="border-t-4">App</div> // top border 4px
+    // <div className="border-r-4">App</div> // right border 4px
+    // <div className="border-b-4">App</div> // bottom border 4px
+    // <div className="border-l-4">App</div> // left border 4px
+    // <div className="border-x-4">App</div> // left and right border 4px
+    // <div className="border-y-4">App</div> // top and bottom border 4px
+    // <div className="border-4 border-red-500">App</div> // 4px red border
+    // <div className="border-4 border-dashed border-red-500">App</div> // 4px red border
+    // <div className="border-4 border-red-500 rounded">App</div> // 4px red border with rounded corners
+    // <div className="border-4 border-red-500 rounded-sm">App</div> // 4px red border with slightly rounded corners
+    // <div className="border-4 border-red-500 rounded-md">App</div> // 4px red border with moderately rounded corners
+    // <div className="border-4 border-red-500 rounded-lg">App</div> // 4px red border with large rounded corners
+    // <div className="border-4 border-red-500 rounded-full">App</div> // 4px red border with fully rounded corners (circle)
+    // <div className="border-4 border-red-500 rounded-tl-lg rounded-br-lg">App</div> // 4px red border with large rounded corners on top-left and bottom-right
+    // <div className="border-4 border-red-500 h-96 w-96">App</div> // 4px red border with height and width of 24rem (96 * 0.25rem)
+    // <div className="border-4 border-red-500 h-[600px] w-[700px]">App</div> // 4px red border with custom height and width of 600px and 700px
+      // <div className="border-4 border-red-500 h-[200px] w-[400px] p-5">App</div> // 4px red border with slightly rounded corners
+    // <div className="border-4 border-red-500 h-[200px] w-[400px] p-6">App</div>
+    // <div className="border-4 border-red-500 h-[200px] w-[400px] pt-10">App</div>
+    // <div className="border-4 border-red-500 h-[200px] w-[400px] pb-10">App</div>
+    // <div className="border-4 border-red-500 h-[200px] w-[400px] pl-10">App</div>
+    // <div className="border-4 border-red-500 h-[200px] w-[400px] pr-10">App</div>
+    // <div className="border-4 border-red-500 h-[200px] w-[400px] m-10">App</div>
+    // <div className="border-4 border-red-500 h-[200px] w-[400px] mt-10">App</div>
+    // <div className="border-4 border-red-500 h-[200px] w-[400px] mb-10">App</div>
+    // <div className="border-4 border-red-500 h-[200px] w-[400px] ml-10">App</div>
+    // <div className="border-4 border-red-500 h-[200px] w-[400px] mr-10">App</div>
+    <div className="border-4 border-green-500 h-50 w-100 m-auto mt-10 bg-[url('assets/image.png')]  bg-center bg-contain bg-no-repeat bg-cover">App</div>
+
+
+
+
+
+  );
+}
+
+export default App;
