667a48304ef0b2aaf1b243bc30aba27052f329db
Ujjwol Tamang <ujjwoltmg47@gmail.com>
2026-04-24 12:52:36 +0545

    tailwind css continue - textandfont, flex, react-icons

--- tailwind_css/src/App.jsx
+++ tailwind_css/src/App.jsx
@@ -1,5 +1,8 @@
+import Header from "./Header";
+
 function App() {
-  return (
+  return(    < Header />
+)
     // <div className="border">App</div> // 1px
     // <div className="border-0">App</div> // 0px
     // <div className="border-2">App</div> // 2px
@@ -22,7 +25,7 @@ function App() {
     // <div className="border-4 border-red-500 rounded-tl-lg rounded-br-lg">App</div> // 4px red border with large rounded corners on top-left and bottom-right
     // <div className="border-4 border-red-500 h-96 w-96">App</div> // 4px red border with height and width of 24rem (96 * 0.25rem)
     // <div className="border-4 border-red-500 h-[600px] w-[700px]">App</div> // 4px red border with custom height and width of 600px and 700px
-      // <div className="border-4 border-red-500 h-[200px] w-[400px] p-5">App</div> // 4px red border with slightly rounded corners
+    // <div className="border-4 border-red-500 h-[200px] w-[400px] p-5">App</div> // 4px red border with slightly rounded corners
     // <div className="border-4 border-red-500 h-[200px] w-[400px] p-6">App</div>
     // <div className="border-4 border-red-500 h-[200px] w-[400px] pt-10">App</div>
     // <div className="border-4 border-red-500 h-[200px] w-[400px] pb-10">App</div>
@@ -33,13 +36,28 @@ function App() {
     // <div className="border-4 border-red-500 h-[200px] w-[400px] mb-10">App</div>
     // <div className="border-4 border-red-500 h-[200px] w-[400px] ml-10">App</div>
     // <div className="border-4 border-red-500 h-[200px] w-[400px] mr-10">App</div>
-    <div className="border-4 border-green-500 h-50 w-100 m-auto mt-10 bg-[url('assets/image.png')]  bg-center bg-contain bg-no-repeat bg-cover">App</div>
-
-
-
-
-
-  );
+    // <div className="border-4 border-green-500 h-50 w-100 m-auto mt-10 bg-[url('assets/image.png')]  bg-center bg-no-repeat bg-cover">App</div>
+    // <div className = "border-2 mt-4 p-3 text-green-400 text-center">Hello world</div>
+    // <div className = "border-2 mt-4 p-3 text-green-400 text-center underline">Hello world</div>
+    // <div className = "border-2 mt-4 p-3 text-green-400 text-center overline">Hello world</div>
+    // <div className = "border-2 mt-4 p-3 text-green-400 text-center line-through">Hello world</div>
+    // <div className = "border-2 mt-4 p-3 text-green-400 text-center uppercase">Hello world</div>
+    // <div className = "border-2 mt-4 p-3 text-green-400 text-center lowercase">Hello world</div>
+    // <div className = "border-2 mt-4 p-3 text-green-400 text-center capitalize text-9xl">Hello world</div>
+    // <div className = "border-2 mt-4 p-3 text-green-400 text-center uppercase">Hello world</div>
+    // <div className="border-4 h-125">
+    //   {/* <div className="border-2 absolute z-10 bg-red-500 top-16 p-3 w-32 h-32">Box1</div>
+    //   <div className="border-2 absolute p-3 w-32 z-9 bg-blue-500 h-32">Box2</div> */}
+    //   <div className="border-2 w-32 h32">Box1</div>
+    //   <div className="border-2 w-32 h32">Box2</div>
+    //   <div className="border-2 w-32 h32">Box3</div>
+    //   <div className="border-2 w-32 h32">Box4</div>
+    //   <div className="border-2 w-32 h32">Box5</div>
+    //   <div className="border-2 w-32 h32">Box6</div>
+    //   <div className="border-2 w-32 h32">Box7</div>
+    //   <div className="border-2 w-32 h32">Box8</div>
+    //   <div className="border-2 w-32 h32">Box9</div>
+    // </div>
 }
 
 export default App;
